e5c9048dccc289c0106545521a6388b8803ec144
Zaharudin Mokhtar <zahratulnazirah@gmail.com>
2023-04-06 22:49:48 +0800

    adjusting API key

--- apps.js
+++ apps.js
@@ -40,7 +40,7 @@ app.post("/",(req,res)=>{
 
     const options = {
         method: "POST",
-        auth:"zahar7:5b43c6b8b00597419ae039177b3b2b4bd-us21"
+        auth:"zahar7:5b43c6b8b00597419ae039177b3b2b4b-us21"
     } 
 
     const request = https.request(url,options, (response)=>{
